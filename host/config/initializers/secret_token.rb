e88b1631853b5f6408b7441286c69bd7e6d7f5fe
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-10-06 20:15:46 +0200

    add rails layout

---
+++ host/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Host::Application.config.secret_token = '6d61d57ac105291d1afb1f61995b159f972225e415eb565482476cb0b81b01cdecf29aeb6ffa0b108eee5196ffbceaf4f3a737d5a550c35864f0d18afc1c16bc'
